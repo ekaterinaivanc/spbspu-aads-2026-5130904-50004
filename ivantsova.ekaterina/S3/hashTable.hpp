13fb3cbee181fc7d9f6ca67c20a30af5b2b01323
ekaterinaivanc <kataivancova7@gmail.com>
2026-05-09 17:03:51 +0300

    Add constructor and destructor

--- ivantsova.ekaterina/S3/hashTable.hpp
+++ ivantsova.ekaterina/S3/hashTable.hpp
@@ -10,65 +10,93 @@
 
 namespace ivantsova {
 
-template< typename T >
-struct Equal {
-  bool operator()(const T& a, const T& b) const {
-    return a == b;
-  }
-};
+  template< typename T >
+  struct Equal {
+    bool operator()(const T& a, const T& b) const {
+      return a == b;
+    }
+  };
 
-struct BoostSHA1Hash {
-  size_t operator()(const std::string& s) const {
-    boost::hash2::sha1_160 sha;
-    sha.update(s.data(), s.size());
-    return boost::hash2::get_integral_result<size_t>(sha);
-  }
-};
+  struct BoostSHA1Hash {
+    size_t operator()(const std::string& s) const {
+      boost::hash2::sha1_160 sha;
+      sha.update(s.data(), s.size());
+      return boost::hash2::get_integral_result<size_t>(sha);
+    }
+  };
 
-template< typename T >
-struct PairHash {
-  size_t operator()(const std::pair<T, T>& p) const {
-    BoostSHA1Hash hasher;
-    return hasher(p.first) ^ (hasher(p.second) << 1);
-  }
-};
+  template< typename T >
+  struct PairHash {
+    size_t operator()(const std::pair<T, T>& p) const {
+      BoostSHA1Hash hasher;
+      return hasher(p.first) ^ (hasher(p.second) << 1);
+    }
+  };
 
-template<typename T>
-struct PairEqual {
-  bool operator()(const std::pair< T, T >& a, const std::pair< T, T >& b) const {
-    return a.first == b.first && a.second == b.second;
-  }
-};
+  template<typename T>
+  struct PairEqual {
+    bool operator()(const std::pair< T, T >& a, const std::pair< T, T >& b) const {
+      return a.first == b.first && a.second == b.second;
+    }
+  };
 
-template< typename Key, typename Value, typename Hash = std::hash< Key >, typename Equal = Equal< Key > >
-class HashTable {
-private:
-  using Bucket = List< std::pair< Key, Value > >;
-  Vector< Bucket > data_;
-  size_t size_;
-  Hash hasher_;
-  Equal equal_;
+  template< typename Key, typename Value, typename Hash = BoostSHA1Hash, typename Equal = Equal< Key > >
+  class HashTable {
+  public:
+    explicit HashTable(size_t slots = 101);
+    ~HashTable();
 
-  size_t getIndex(const Key& key) const {
-    return hasher_(key) % data_.getSize();
-  }
+    HashTable(const HashTable& other);
+    HashTable(HashTable&& other) noexcept;
+    HashTable& operator=(const HashTable& other);
+    HashTable& operator=(HashTable&& other) noexcept;
 
-public:
-  explicit HashTable(size_t slots = 101):
-    data_(slots), size_(0)
-  {}
+    void add(const Key& key, const Value& value);
+    Value drop(const Key& key);
+    Value& get(const Key& key);
+    const Value& get(const Key& key) const;
+    bool has(const Key& key) const noexcept;
+    void rehash(size_t new_slots);
 
-  size_t size() const noexcept {
-    return size_;
-  }
-  bool empty() const noexcept {
-    return size_ == 0;
-  }
-  size_t bucket_count() const noexcept {
-    return data_.getSize();
-  }
-};
+    void clear() noexcept;
+    size_t size() const noexcept;
+    bool empty() const noexcept;
+    void swap(HashTable& other) noexcept;
 
+  private:
+    using Bucket = List< std::pair< Key, Value > >;
+    Vector< Bucket > data_;
+    size_t size_;
+    Hash hasher_;
+    Equal equal_;
+
+    size_t getIndex(const Key& key) const;
+  };
+}
+
+template< typename Key, typename Value, typename Hash, typename Equal >
+size_t ivantsova::HashTable< Key, Value, Hash, Equal >::getIndex(const Key& key) const {
+  return hasher_(key) % data_.getSize();
+}
+
+template< typename Key, typename Value, typename Hash, typename Equal >
+ivantsova::HashTable< Key, Value, Hash, Equal >::HashTable(size_t slots):
+  data_(slots), size_(0)
+{}
+
+template< typename Key, typename Value, typename Hash, typename Equal >
+ivantsova::HashTable< Key, Value, Hash, Equal >::~HashTable() {
+  clear();
+}
+
+template< typename Key, typename Value, typename Hash, typename Equal >
+size_t ivantsova::HashTable< Key, Value, Hash, Equal >::size() const noexcept {
+  return size_;
+}
+
+template< typename Key, typename Value, typename Hash, typename Equal >
+bool ivantsova::HashTable< Key, Value, Hash, Equal >::empty() const noexcept {
+  return size_ == 0;
 }
 
 #endif
