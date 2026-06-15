2a6bfa201a83af8aec8522bf6ef7dbc81c5ad3e6
ekaterinaivanc <kataivancova7@gmail.com>
2026-06-15 16:58:37 +0400

    Add struct user

---
+++ ivantsova.ekaterina/F0/user.hpp
@@ -0,0 +1,37 @@
+#ifndef USER_HPP
+#define USER_HPP
+
+#include <string>
+#include "vector.hpp"
+#include "hash_table.hpp"
+
+namespace ivantsova {
+
+  struct User
+  {
+    std::string id;
+    std::string lastName;
+    std::string firstName;
+    std::string city;
+    std::string birthday;
+    MyVector< std::string > friends;
+
+    User();
+    User(const std::string& uid, const std::string& ln,
+      const std::string& fn, const std::string& c, const std::string& bd);
+    bool operator==(const User& other) const;
+  };
+
+  class GlobalUserRegistry
+  {
+  private:
+    DoubleHashTable< std::string, User > users;
+  public:
+    bool registerUser(const User& user);
+    bool unregisterUser(const std::string& id);
+    User* findUser(const std::string& id);
+    bool userExists(const std::string& id) const;
+  };
+}
+
+#endif
