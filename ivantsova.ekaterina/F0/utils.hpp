90aa82c0cbb9b669769609eb0919c8c02bd1b011
ekaterinaivanc <kataivancova7@gmail.com>
2026-06-15 17:05:53 +0400

    Create utils

---
+++ ivantsova.ekaterina/F0/utils.hpp
@@ -0,0 +1,15 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <string>
+
+namespace ivantsova {
+
+  bool isValidBirthday(const std::string& date);
+  int calculateAge(const std::string& birthday);
+  void trim(std::string& str);
+  bool isLeapYear(int year);
+  int daysInMonth(int month, int year);
+}
+
+#endif
